7260ac9212ac536f2ba8378b823843a4e91e1b71
vanshaggrwal <101628876+vanshaggrwal@users.noreply.github.com>
2024-08-12 22:45:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import { Component } from 'react';
+import './App.css';
+import Game from './components/Game';
+
+class App extends Component{  
+    render(){
+      return (
+      <Game/>
+    );
+  }
+}
+
+export default App;
